6bb59962cc589d6a1545ade8f1ec737450214044
Patrick Morpey <patrick@morphp.nl>
2022-08-25 10:28:48 +0200

    Added printText() and printEol() to the Printer interface

--- src/Printer.php
+++ src/Printer.php
@@ -7,4 +7,8 @@ namespace Medas\Console;
 interface Printer
 {
     public function print(Printable ...$blocks): self;
+
+    public function printText(string $text, mixed $format = null): self;
+
+    public function printEol(): self;
 }
