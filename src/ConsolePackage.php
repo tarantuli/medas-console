1e422a5ab0c543950ce92a8541c594099f061c13
Patrick Morpey <patrick@morphp.nl>
2023-05-02 09:25:16 +0200

    Updated packages

--- src/ConsolePackage.php
+++ src/ConsolePackage.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Medas\Console;
 
-use Medas\ServiceManager\{AsSingleton, BasePackage};
+use Medas\Core\AsSingleton;
+use Medas\ServiceManager\BasePackage;
 
 class ConsolePackage extends BasePackage
 {
