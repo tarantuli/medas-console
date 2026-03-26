80c8331f23c6242d0837b4b5a49ef8ff7f4402d0
Patrick Morpey <patrick@morphp.nl>
2026-03-26 15:53:41 +0100

    Added argument and option helper methods to `CommandInput` class

--- src/Commands/CommandInput.php
+++ src/Commands/CommandInput.php
@@ -13,10 +13,39 @@ readonly class CommandInput
         public array $arguments,
 
         /**
-         * An associative array of options passed to the command. A value of null indicates that no value was passed.
+         * An associative array of options passed to the command. A value of true indicates that no value was passed.
          */
         public array $options,
     )
     {
+    }
+
+    /**
+     * Returns if the given zero-based index is set.
+     */
+    public function hasArgument(int $index): bool
+    {
+        return array_key_exists($index, $this->arguments);
+    }
+
+    /**
+     * Returns the value of the argument at the given zero-based index, null if it is not set.
+     */
+    public function getArgument(int $index): mixed
+    {
+        return $this->arguments[$index] ?? null;
+    }
+
+    public function hasOption(string $name): bool
+    {
+        return array_key_exists($name, $this->options);
+    }
+
+    /**
+     * Returns the value of the option with the given name, null if it is not set.
+     */
+    public function getOption(string $name): mixed
+    {
+        return $this->options[$name] ?? null;
     }
 }
