df6edac89a682ae1d4b54f3c80a5d6adf84a1ed2
Patrick Morpey <patrick@morphp.nl>
2026-05-10 15:47:16 +0200

    Added `Tree` class implementation with support for nodes, labels, and children

---
+++ src/Tree.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\Console;
+
+/**
+ * A tree that consists of nodes with labels and children
+ */
+class Tree implements Printable
+{
+    public function __construct(
+        public iterable $nodes,
+
+        /** @param Closure(mixed): Printable|string $label */
+        public \Closure $label,
+
+        /** @param Closure(mixed): iterable&\Countable $children */
+        public \Closure $children,
+    )
+    {
+    }
+}
