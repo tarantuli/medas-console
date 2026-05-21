773e9f5e58e4fbabfb2702fb6c25e4b72ceb4aef
Patrick Morpey <patrick@morphp.nl>
2026-05-21 08:46:18 +0200

    Removed unused service manager initialization logic from `phpunit.bootstrap.php`.

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -1,18 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-use Medas\Console\ConsolePackage;
-use Medas\ServiceManager\{ServiceConfig, ServiceManager};
-
-chdir(__DIR__);
-
-new ServiceManager(function (): ServiceConfig {
-    $config = new ServiceConfig();
-
-    $config->addPackages([
-        ConsolePackage::instance(),
-    ]);
-
-    return $config;
-});
